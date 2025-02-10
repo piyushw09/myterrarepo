157d6cc2995be8e7c74810caed0ac862bcb46cb7
piyushw09 <piyushw0622@gmail.com>
2025-02-10 18:59:41 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
  
   ami           = var.my_ami
-  instance_type = t2.micro
+  instance_type = "t2.micro"
 
   tags = {
     Name = "myinstance "
